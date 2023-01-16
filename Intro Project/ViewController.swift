8b0a4223e3f448c1cebbe666fde0518113d2727b
Ailany <88301756+ailanyEXE@users.noreply.github.com>
2023-01-16 15:23:40 -0800

    Initial Commit

---
+++ Intro Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intro Project
+//
+//  Created by Ailany on 1/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
